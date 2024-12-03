5dd7cd7b57af9e67e5f152fba6a153da2a1548e5
dchizhikov <dchizhikov@gmail.com>
2024-12-03 10:44:04 +0000

    tg_bot test-0.1 commit message

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
-import tg_bot.config as config
 import importlib
 import os
+import sys
+
+sys.path.append(f'/content/tg_bot')
+#config = importlib.import_module('config')
+sys.path.append(f'/content/tg_bot/modules/')
+
+os.system('pip install pyTelegramBotAPI')
 
 print("Начало")
 
 folder_modules = 'modules'
-folder_modules_path = config.myRepo+'/' +folder_modules
+folder_modules_path = '/content/tg_bot'+'/' +folder_modules #config.myRepo+'/' +folder_modules
 modules_list = [os.path.splitext(file)[0] for file in os.listdir(folder_modules_path) if file.endswith('.py')]
 imported_modules = {}
 
 # Импортируем и перезагружаем модули
 for module in modules_list:
     # Динамический импорт модуля
-    imported_module = importlib.import_module(f"{config.folder_name}.modules.{module}")
-    importlib.reload(imported_module)
+    imported_module = importlib.import_module(module)
+#    importlib.reload(imported_module)
     imported_modules[module] = imported_module
 
 gc = imported_modules['git_com']
 tg = imported_modules['tg_neuro_net_bot']
 
 tg.my_function()
-tg.bot_polling()
+#tg.bot_polling()
 print("Конец")
